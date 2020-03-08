11445eebb60c7583e2ede3952b968cdfc4558226
sun <chentao980225@163.com>
2020-03-08 20:32:36 +0800

    组件间通信(消息订阅)

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,12 @@ import Search from './components/search/Search'
 import List from './components/list/List'
 
 export default class App extends Component {
-    state={
-        keyWord:''
-    }
-    updateKeyWord=(keyWord)=>{
-        this.setState({
-            keyWord
-        })
-    }
+
     render() {
         return (
             <div className="container">
-                <Search updateKeyWord={this.updateKeyWord}/>
-                <List keyWord={this.state.keyWord}/>
+                <Search/>
+                <List/>
             </div>
         )
     }
