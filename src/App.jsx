08c14d80f6e39b50364f0f33e92c3d767ac93311
sun <chentao980225@163.com>
2020-03-08 08:55:24 +0800

    searchUser

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,22 @@
 import React, {Component} from 'react'
+import Search from './components/search/Search'
+import List from './components/list/List'
+
 export default class App extends Component {
     state={
-        isLoading:true,
-        repoName:'',
-        repoUrl:'',
-        errMsg:''
+        keyWord:''
     }
-    keyWord='v'
-    async componentDidMount(){
-        const url= `https://api.github.com/search/repositories?q=${this.keyWord}&sort=stars`
-        fetch(url)
-            .then((result)=>{
-                if(result.ok){
-                    return result.json()
-                }else{
-                    return Promise.reject('请求资源不存在')
-                }
-            })
-            .then((data)=>{
-                console.log('成功了')
-                let repoInfo=data.items[0]
-                this.setState({
-                    isLoading:false,
-                    repoName:repoInfo.name,
-                    repoUrl:repoInfo.html_url,
-                    errMsg:''
-                })
-            })
-            .catch((err)=>{
-                console.log(err.toString())
-                this.setState({
-                    isLoading:false,
-                    repoName:'',
-                    repoUrl:'',
-                    errMsg:err.toString()
-                })
-            })
+    updateKeyWord=(keyWord)=>{
+        this.setState({
+            keyWord
+        })
     }
-
     render() {
-        let {isLoading,repoUrl,errMsg,repoName}=this.state;
-        if(isLoading){
-            return <h3>loading...</h3>
-        }else if(errMsg){
-            return <h3>{errMsg}</h3>
-        }else{
-            return <h3>github上包含【{this.keyWord}】关键字的所有仓库中，点赞数量最多的是 <a href={repoUrl}>{repoName}</a></h3>
-        }
-
+        return (
+            <div className="container">
+                <Search updateKeyWord={this.updateKeyWord}/>
+                <List keyWord={this.state.keyWord}/>
+            </div>
+        )
     }
 }
